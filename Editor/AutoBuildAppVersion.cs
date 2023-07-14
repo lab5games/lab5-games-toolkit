c1371ebeeb386b2cfcdde46330fc72e6b1831644
CastleWSC <castle.wsc@gmail.com>
2023-07-14 20:39:24 +0800

    update 1.0.4

--- Editor/AutoBuildAppVersion.cs
+++ Editor/AutoBuildAppVersion.cs
@@ -8,6 +8,10 @@ using System.IO;
 
 namespace Lab5Games.Editor
 {
+    // https://blog.csdn.net/weixin_44389880/article/details/113699329
+    // https://www.cnblogs.com/andrew-blog/archive/2012/08/21/SVN_DOS_Commands.html
+    // https://itmonon.blog.csdn.net/article/details/127475196?spm=1001.2101.3001.6650.1&utm_medium=distribute.pc_relevant.none-task-blog-2%7Edefault%7ECTRLIST%7ERate-1-127475196-blog-113699329.235%5Ev38%5Epc_relevant_sort_base2&depth_1-utm_source=distribute.pc_relevant.none-task-blog-2%7Edefault%7ECTRLIST%7ERate-1-127475196-blog-113699329.235%5Ev38%5Epc_relevant_sort_base2&utm_relevant_index=2&ydreferer=aHR0cHM6Ly9ibG9nLmNzZG4ubmV0L3dlaXhpbl80NDM4OTg4MC9hcnRpY2xlL2RldGFpbHMvMTEzNjk5MzI5
+
     public class AutoBuildAppVersion : IPostprocessBuildWithReport
     {
         public int callbackOrder { get; }
@@ -68,23 +72,26 @@ namespace Lab5Games.Editor
 
         void SvnVersionControl()
         {
+            string controlPaths =
+                ToolkitSettings.AssetPath + "*" +
+                Path.Combine(Environment.CurrentDirectory, "ProjectSettings/ProjectSettings.asset");
+
             // update unity ProjectSettings folder
             UnityEngine.Debug.Log("[AutoBuildAppVersion] Svn update...");
 
             Process.Start(
                 "TortoiseProc.exe",
-                "/command:update /path:" +
-                Path.Combine(Environment.CurrentDirectory, "ProjectSettings") +
+                "/command:update /path:\"" + controlPaths + "\"" + 
                 " /closeonend:1");
+                
 
             // commit untiy ProjectSettings folder
             UnityEngine.Debug.Log("[AutoBuildAppVersion] Svn commit...");
 
             Process.Start(
                 "TortoiseProc.exe",
-                "/command:commit /path:" +
-                Path.Combine(Environment.CurrentDirectory, "ProjectSettings") +
-                $" /logmsg 'auto_version_{PlayerSettings.bundleVersion}'" + 
+                "/command:commit /path:\"" + controlPaths + "\"" +
+                $" /logmsg \"" + $"auto version {PlayerSettings.bundleVersion}" + "\"" +  
                 " /closeonend:1");
         }
 
